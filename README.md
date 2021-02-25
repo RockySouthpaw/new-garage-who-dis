90dd771aeeec1253a76d7d9101d2ef0eb198b1c8
Darrian <darriangaylor@icloud.com>
2021-02-25 00:34:43 -0500

    Initial commit (Folder structure)

--- README.md
+++
@@ -1,1 +0,0 @@
-# new-garage-who-dis
---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+description 'New-Garage-Who-Dis'
+
+server_scripts {
+	'server/sv_main.lua',
+	'config/sv_config.lua'
+}
+
+client_scripts {
+	'client/cl_main.lua',
+	'config/cl_config.lua'
+}
