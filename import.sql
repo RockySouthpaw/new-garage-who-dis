7c0f32738e54ad8f0825807ff7858631fef1b8b9
Darrian <darriangaylor@icloud.com>
2021-03-13 03:09:16 -0500

    Mock sql. (Unfinished)

--- import.sql
+++ import.sql
@@ -1,11 +1,7 @@
-CREATE TABLE `ngwd_garage` (
-	`identifier` VARCHAR(48) NOT NULL,
-	`plate` varchar(12) NOT NULL,
-	`vehicle` longtext,
-	`class` varchar(15) NOT NULL,
-	`Engine Health` varchar(5) NOT NULL DEFAULT '1000',
-	`Body Health` varchar(5) NOT NULL DEFAULT '1000',
-	`Tank Health` varchar(5) NOT NULL DEFAULT '1000',
-	`garage_location` varchar(60) NOT NULL DEFAULT 'Garage_1',
-	PRIMARY KEY (`identifier`)
+CREATE TABLE `ngwd_vehicle` (
+    `id` Int(11) AUTO_INCREMENT NOT NULL,
+    `owner` VARCHAR(48) NOT NULL,
+    `plate` varchar(12) NOT NULL,
+    `garage_location` varchar(60) NOT NULL DEFAULT 'Garage_1',
+    PRIMARY KEY (`id`)
 );
