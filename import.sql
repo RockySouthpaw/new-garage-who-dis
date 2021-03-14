110226a528f9be03ad45134609b09cd57eb7d62d
Darrian <darriangaylor@icloud.com>
2021-03-14 03:09:16 -0400

    RFR: Adjusted default location since we use blip names.

--- import.sql
+++ import.sql
@@ -2,6 +2,6 @@ CREATE TABLE `ngwd_vehicle` (
     `id` Int(11) AUTO_INCREMENT NOT NULL,
     `owner` VARCHAR(48) NOT NULL,
     `plate` varchar(12) NOT NULL,
-    `garage_location` varchar(60) NOT NULL DEFAULT 'Garage_1',
+    `garage_location` varchar(60) NOT NULL DEFAULT 'Legion',
     PRIMARY KEY (`id`)
 );
