7e39f7122cba84f04119f43fca0623aa67c4d858
Darrian <darriangaylor@icloud.com>
2021-11-01 21:21:20 -0400

    fix: revert modelClass to int

--- import.sql
+++ import.sql
@@ -3,7 +3,7 @@ CREATE TABLE `ngwd_vehicles` (
     `owner` varchar(48) NOT NULL,
     `modelName` varchar(55) DEFAULT NULL,
     `modelHash` Int(11) NOT NULL,
-    `modelClass` varchar(55) DEFAULT NULL,
+    `modelClass` Int(2) NOT NULL,
     `localizedName` varchar(55) DEFAULT NULL,
     `plate` varchar(12) NOT NULL,
     `garage` varchar(60) DEFAULT 'Legion',
